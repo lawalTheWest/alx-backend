f66091d50db3484c68422a83e3b05f6c01a9693f
lawalTheWest <lwltjdn@gmail.com>
2024-07-20 18:41:41 +0100

    documented the function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,4 +9,7 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''
+        returns a tuple
+    '''
     return ((page -1) * page_size, page * page_size)
